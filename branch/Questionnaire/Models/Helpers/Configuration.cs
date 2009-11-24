556bf1a4ed396f3c531355cba5efddfb07280c43
matthewchoinski@gmail.com <matthewchoinski@gmail.com@659e5328-d8fe-11de-8d17-8328ca00869f>
2009-11-24 18:50:13 +0000

    Creating development branch.

---
+++ branch/Questionnaire/Models/Helpers/Configuration.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Configuration;
+
+namespace Questionnaire.Models
+{
+    internal static class Configuration
+    {
+        internal static string DataStoreName
+        {
+            get { return ConfigurationSettings.AppSettings[ "DataStoreName" ]; }
+        }
+
+        internal static int NumberOfQuestionsToDisplay
+        {
+            get
+            {
+                int result = 0;
+                int.TryParse (
+                    ConfigurationSettings.AppSettings[ "NumberOfQuestionsToDisplay" ],
+                    out result );
+                return result;
+            }
+        }
+    }
+}
---
+++ branch/Questionnaire/Models/Helpers/DataStore.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+using Context = System.Web.HttpContext;
+
+namespace Questionnaire.Models
+{
+    internal class DataStore
+    {
+        internal static string Fetch ( )
+        {
+            string result = string.Empty;
+
+            string dataStoreFileSystemPath =
+                Context.Current.Request.PhysicalApplicationPath
+                + @"Models\DataStores\" 
+                + Configuration.DataStoreName
+                + @".txt";
+            StreamReader streamReader = null;
+            try
+            {
+                streamReader = 
+                    new StreamReader ( dataStoreFileSystemPath );
+                result = streamReader.ReadToEnd ( );
+            }
+            finally
+            {
+                streamReader.Close ( );
+            }
+
+            return result;
+        }
+    }
+}
