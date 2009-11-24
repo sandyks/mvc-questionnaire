0ac694613f9165d0e038da484638baed5f149bff
matthewchoinski <matthewchoinski@gmail.com>
2009-11-24 18:47:23 +0000

    Initial check-in.

---
+++ Questionnaire/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using Context = System.Web.HttpContext;
+
+namespace Questionnaire.Models
+{
+    public class Person
+    {
+        public static string Name
+        {
+            get
+            {
+                return Context.Current.User.Identity.Name;
+            }
+        }
+    }
+}
