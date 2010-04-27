1c76c58574101fa65025acc98053fdb0713979fd
matthewchoinski <matthewchoinski@659e5328-d8fe-11de-8d17-8328ca00869f>
2010-04-27 02:24:11 +0000

    Updated the project with the Message object.

---
+++ Questionnaire/Models/Message.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Configuration;
+using System.Net.Mail;
+
+namespace Questionnaire.Models
+{
+    public class Message
+    {
+        public void Send ( 
+            string personName, 
+            int numberOfCorrect, 
+            int numberOfIncorrect )
+        {
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From =
+                new MailAddress( Configuration.MessageSender );
+            mailMessage.Subject = "Questionnaire Results for " + personName;
+            mailMessage.Body =
+                string.Format(
+                    "{0}\n\nNumber Of Correct Questions: {1}\nNumber Of Incorrect Questions: {2}",
+                    personName,
+                    numberOfCorrect,
+                    numberOfIncorrect );
+            mailMessage.To.Add( Configuration.MessageRecipient );
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = Configuration.MessageServer;
+            smtpClient.Send( mailMessage );
+        }
+    }
+}
